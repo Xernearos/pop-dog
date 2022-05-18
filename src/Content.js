c4c0cfb8bc63832c9e59675259450268037c5b63
TerrifyGary <yoyoman1415926@gmail.com>
2022-05-18 17:14:34 +0800

    新增： 1. 將count參數，與網頁端坐連接。 2. 記得，<TouchableOpacity> 是 React 的 Button。

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,32 @@
-import React,{Component} from "react"
+import React, { useEffect, useState } from "react";
+import { View, TouchableOpacity } from 'react-native';
 import cat from './cat-350-467.png'
 import './Content.css'
 
-var clicks = 0;
-function myfunction(){
-    clicks+=1;
-    console.log(clicks);
-}
 export default ({ children }) => (
     <div className='Content'>
-        <button onClick={myfunction}>
-            <img src={cat}/> 
-        </button>
-        <a class=''>Click Times = {clicks} </a>
+        <Counter></Counter>
     </div>
 )
+
+const Counter = () => {
+    const [count, setCount] = useState(0);   
+    return (
+        <View>
+            <TouchableOpacity 
+                style={{flexDirection:"row",alignItems:'center',justifyContent:'center'}}
+                onPress={() => {setCount(count + 1)}}    
+            >
+                <img className="Image" src={cat}/> 
+            </TouchableOpacity>
+            <a 
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+                className='Word'
+                >Click Times = {count} </a>
+        </View>
+    );
+}

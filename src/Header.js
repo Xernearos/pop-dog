803637b4fe856446f4cb44d17f1e4d3e577efcc9
TerrifyGary <yoyoman1415926@gmail.com>
2022-05-20 16:57:59 +0800

    新增： 1. 加入首頁 Header 上方的 回傳隊伍。 2. 修正 Content 的圖片大小。

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,39 @@
-import React from "react"
 import "./Header.css"
+import React, { useEffect, useState } from "react";
+import { Text, View, TouchableOpacity } from 'react-native';
 
 export default ({ children }) => (
     <div className='Header'>
-        <a>Choose Team :</a>
-        <div className='Teams'>
-            <button onClick={console.log('Team Red.')}>
-                <a style={{color:'#FF7F7F'}}>Red</a>
-            </button>
-        </div>
-        <div className='Teams'>
-            <button onClick={console.log('Team Blue.')}>
-                <a style={{color:'#ADD8E6'}}>Blue</a>
-            </button>           
-        </div>
-        <div className='Teams'>
-            <button onClick={console.log('Team Yellow.')}>
-                <a style={{color:'#ffff80'}}>Yellow</a>
-            </button>
-        </div>
+        <Team></Team>
     </div>
+    
 )
+
+const Team = () =>{
+    const [team, setTeam] = useState('Red');
+    return(
+        <View style={{display:'flex', flexDirection:'row',alignItems:'center', justifyContent:'center'}}>
+            <a style={{padding:'0px 30px 0px 0px'}}>Choose Team :</a>
+            <TouchableOpacity 
+                onPress={() => {setTeam("Red")}}
+                style={{color:'#FF7F7F',flexWrap:'wrap',padding:'10px',margin:'10px'}}>
+                Red
+            </TouchableOpacity>
+            <TouchableOpacity 
+                onPress={() => {setTeam("Blue")}}
+                style={{color:'#ADD8E6',flexWrap:'wrap',padding:'10px',margin:'10px'}}>
+                Blue
+            </TouchableOpacity>
+            <TouchableOpacity 
+                onPress={() => {setTeam("Yellow")}}
+                style={{color:'#ffff80',flexWrap:'wrap',padding:'10px',margin:'10px'}}>
+                Yellow
+            </TouchableOpacity>
+            <Text
+                style={{padding:'0px 0px 0px 30px',color:'white',fontSize:'15px'}}>
+                Now : {team}
+            </Text>
+        </View>
+    )
+    
+}
